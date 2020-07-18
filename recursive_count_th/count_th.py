33d0f8b29e366f797c8e72cff93ffce0bbedf5f0
Danny Town <47186976+dannyotown@users.noreply.github.com>
2020-07-18 12:53:06 -0500

    Robot sort and recursive function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,18 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+cache = {'count': 0}
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    if len(word) < 2:
+        val = cache['count']
+        cache['count'] = 0
+        return val
+    elif (word[-2:]) == 'th':
+        cache['count'] += 1
+    return count_th(word[:-1])
+
+
+print(count_th('abcthefthghith'))
